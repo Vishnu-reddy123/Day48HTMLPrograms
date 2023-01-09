b6ecfc18f9f9dd1c99838d794e227c1afabdfb34
Vishnu-reddy123 <vishnu95532@gmail.com>
2023-01-09 11:54:00 +0530

    added logic for converting employee payroll app to client server architecture

--- js/payrollform.js
+++ js/payrollform.js
@@ -1,3 +1,5 @@
+//defining global variables in order to update the details
+//isUpdate to check for true or false whether update was done or not
 let isUpdate=false;
 //creating employeepayrollobj (Json) whiich will be used to update the employee details in local storage
 let employeePayrollObj={};
@@ -84,42 +86,17 @@ const save=(event)=>{
         //major refactoring of code is done here to to save updated employees
         //calling set employeepayroll object which adds value in payroll form to employee payroll obj json
         setEmployeePayrollObject(); 
-        if(site_properties.use_local_storage.match("true"))
-        {
-            //after adding values, create and update storage is called where values are added into local storage or updated
-            createAndUpdateStorage();
-            resetForm();
-            //after resetting, moving back to home page.
-            window.location.replace(site_properties.home_page);
-        }
-        else createOrUpdateEmployeePayroll();
+        //after adding values, create and update storage is called where values are added into local storage or updated
+        createAndUpdateStorage();
+        resetForm();
+        //after resetting, moving back to home page.
+        window.location.replace(site_properties.home_page);
     }
     catch(e)
     {
         return;
     }
 
-}
-
-const createOrUpdateEmployeePayroll=()=>
-{
-    let postURL= site_properties.server_url;
-    let methodCall="POST";
-    if(isUpdate)
-    {
-        methodCall="PUT";
-        postURL=postURL+employeePayrollObj.id.toString();
-    }
-    makeServiceCall(methodCall,postURL,true,employeePayrollObj)
-        .then(responseText=>
-            {
-                resetForm();
-                window.location.replace(site_properties.home_page);                
-            })
-        .catch(error=>
-            {
-                throw error;
-            })
 }
 
 //setting employee payroll objects with data entered in payroll form
--- js/payrollhome.js
+++ js/payrollhome.js
@@ -1,40 +1,25 @@
+//adding a global employee payroll list-array which will contain objects read from local storage
+//hence will be used in populating table on webpage
 let empPayrollList;
 //creating event listener which will be instatiated once all the content is loaded on webpage
 window.addEventListener('DOMContentLoaded',(event)=>
 {
-    if(site_properties.use_local_storage.match("true"))
-    {
-        getEmployeePayrollDataFromStorage();
-    }
-    else
-        getEmployeePayrollDataFromServer();
+    //for adding data into arrray, calling method get Employee payroll data from storage
+    empPayrollList= getEmployeePayrollDataFromStorage();
+    //printing out the count of employees in list or table using emp payroll list length attribute and class name in html.
+    document.querySelector(".emp-count").textContent= empPayrollList.length;
+    //calling method to add data into the table
+    createInnerHtml();
+    //removing item from editEmp, so as to add new item to edit emp for updating items
+    localStorage.removeItem('editEmp');
 });
 //function defined in form of array functions to get data from local storage
 //in array function, ,condition is checked, if particular key (keys are there in local storage and data is added corresponding to it, here our
 //key is EmployeePayrollList) is there in database, if there all data is read form localstorage through getItem method, else empty array is returned.
 //data is added in local storage in form of strings. json.parse converts local storage in object of Employeepayroll, in the form it was added.
 const getEmployeePayrollDataFromStorage= ()=>{
-    empPayrollList= localStorage.getItem('EmployeePayrollList')?JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
-    processEmployeePayrollDataResponse();
-}
-const processEmployeePayrollDataResponse=()=>{
-    document.querySelector(".emp-count").textContent=empPayrollList.length;
-    createInnerHtml();
-    localStorage.removeItem('editEmp');
-}
-
-const getEmployeePayrollDataFromServer=()=>
-{
-    makeServiceCall("GET",site_properties.server_url,true)
-        .then(responseText=>{
-            empPayrollList= JSON.parse(responseText);
-            processEmployeePayrollDataResponse();
-        })
-        .catch(error=>{
-            console.log("GET Error Status: "+JSON.stringify(error));
-            empPayrollList=[];
-            processEmployeePayrollDataResponse();
-        })
+    
+    return localStorage.getItem('EmployeePayrollList')?JSON.parse(localStorage.getItem('EmployeePayrollList')):[];
 }
 
 //method to add data into inner html which adds data into the table
@@ -63,8 +48,8 @@ const createInnerHtml=()=>
             </td>
             <td>${empPayrollData._salary}</td>
             <td>${stringifyDate(empPayrollData._startDate)}</td>
-            <td><img id="${empPayrollData.id}" onclick= "remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-            <img id="${empPayrollData.id}" onclick= "update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg"></td>
+            <td><img id="${empPayrollData.id}" onclick= "remove(this)" alt="delete" src="../profilePic/delete-black-18dp.svg">
+            <img id="${empPayrollData.id}" onclick= "update(this)" alt="edit" src="../profilePic/create-black-18dp.svg"></td>
         </tr>`;
     }
     //displaying the data using innerHTML
@@ -76,7 +61,7 @@ const createInnerHtml=()=>
 const createEmployeePayrollJSON = () => {
     let empPayrollListLocal = [
       {       
-        _name: 'Akash',
+        _name: 'Akshay',
         _gender: 'male',
         _department: [
             'Engineering',
@@ -86,7 +71,7 @@ const createEmployeePayrollJSON = () => {
         _startDate: '29 Oct 2019',
         _note: '',
         id: new Date().getTime(),
-        _profilePic: '../assets/profile-images/Ellipse -2.png'
+        _profilePic: '../profilePic/Ellipse -2.png'
       },
       {
         _name: 'Kumar',
@@ -98,7 +83,7 @@ const createEmployeePayrollJSON = () => {
         _startDate: '29 Oct 2019',
         _note: '',
         id: new Date().getTime() + 1,
-        _profilePic: '../assets/profile-images/Ellipse -1.png'
+        _profilePic: '../profilePic/Ellipse -1.png'
       }
     ];
     return empPayrollListLocal;
@@ -128,27 +113,11 @@ const createEmployeePayrollJSON = () => {
       const index= empPayrollList.map(empData=>empData.id).indexOf(empPayrollData.id);
       //using splice to remove element from array
       empPayrollList.splice(index,1);
-      if(site_properties.use_local_storage.match("true"))
-      {
       //updating the data into local storage
       localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
       //updating the count of employees here, otherwise refresh will be required to update count
       //refresh slows the code, hence update of count is done here only.
       document.querySelector(".emp-count").textContent= empPayrollList.length;
-      }
-      else
-      {
-          const deleteURL= site_properties.server_url+empPayrollData.id.toString();
-          makeServiceCall("DELETE",deleteURL,false)
-          .then(responseText=>
-            {
-                document.querySelector(".emp-count").textContent= empPayrollList.length;
-                createInnerHtml();
-            })
-            .catch(error=>{
-                console.log("DELETE Error status: "+JSON.stringify(error));
-            })
-      }
       //showing updated data of local storage
       createInnerHtml();
   }
